e900b6313f46394b5281632aef38f0b0780e1a09
didilovu <124969658+didilovu@users.noreply.github.com>
2023-10-19 20:12:54 +0700

    Add files via upload

---
+++ mod4/ex02/learning_tf2_py/launch/turtle_tf2_dynamic_frame_demo.launch.py
@@ -0,0 +1,33 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+from launch_ros.actions import SetParameter
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    demo_nodes = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('learning_tf2_py'), 'launch'),
+            '/turtle_tf2_demo.launch.py']),
+       launch_arguments={'target_frame': 'carrot1'}.items(),
+       )
+
+    return LaunchDescription([
+        SetParameter(name='radius', value=4),
+        SetParameter(name='direction_of_rotation', value=1),
+        demo_nodes,
+        Node(
+            package='learning_tf2_py',
+            executable='dynamic_frame_tf2_broadcaster',
+            name='dynamic_broadcaster',
+            arguments=['--radius', '5', '--direction_of_rotation', '1']
+        ),
+    ])
